5b4d25f417892f0651fc46fb2e84aa6d1cb62a78
vikashCoach <vikkumar@tesla.com>
2021-07-19 17:17:37 -0700

    test#1

---
+++ LocalCinema.Core.Services/Interfaces/ICustomerManager.cs
@@ -0,0 +1,11 @@
+﻿using LocalCinema.Data.Model;
+using System.Threading.Tasks;
+
+namespace LocalCinema.Core.Services.Interfaces
+{
+    public interface ICustomerManager
+    {
+        Task<OperationResult> GetMovieTimes(string movieName,string date);
+        Task<OperationResult> GetMovieDetails(string movieName);
+    }
+}
